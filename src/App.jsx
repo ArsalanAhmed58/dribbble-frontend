b49d6ed41e6d77c44dd68ba64a51cafb5c26ae83
Arsalan58 <arsalanahmed5828@gmail.com>
2024-04-13 11:24:05 +0530

    na

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import "./App.css"
 // import SignUp from "./pages/SignUp"
 import {Routers} from "./routes"
-import WhyDribbble from "./pages/WhyDribbble";
 const App = () => {
   return (
-    // <Routers/>
-      <WhyDribbble/>
+    <Routers/>
   )
 }
 
