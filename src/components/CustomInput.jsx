cf6bcd4247c90996a6cac18923b62fcb868b2e91
Arsalan58 <arsalanahmed5828@gmail.com>
2024-04-13 11:11:06 +0530

    na

--- src/components/CustomInput.jsx
+++ src/components/CustomInput.jsx
@@ -14,7 +14,6 @@ const CustomInput = ({touched, error, value, name = "email", onChange, label, fu
                         marginTop: 3,
                     },
                     '&:focus': {
-                        borderColor: "rgba(0, 0, 0, 0.1)",
                         boxShadow: "0 0 0 4px rgba(234,100,217,0.1)",
                         borderColor: "rgba(234, 100, 217, 0.4)", outline: "none",
                     },
